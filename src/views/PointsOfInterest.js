ee76f221fea137bcf5f56a91b25894d6c23b2c0a
VMaderaAlbors <148540161+VMaderaAlbors@users.noreply.github.com>
2024-08-08 21:08:43 +0100

    Update PointsOfInterest.js

--- src/views/PointsOfInterest.js
+++ src/views/PointsOfInterest.js
@@ -4,7 +4,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { APIProvider } from '@vis.gl/react-google-maps';
 import GetPOI from '../components/GetPOI';
 function PointsOfInterest() {
-    const API_KEY = 'AIzaSyA6y0oyQCZhjfQtsxnq-GQNWlF24cp0p80';
+    
     return (
         <div>
 
